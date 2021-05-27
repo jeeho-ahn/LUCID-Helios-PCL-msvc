c4a8e506718fe201f249f118563243e5d80be1ef
Jeeho Ahn <jeeho@kist.re.kr>
2021-05-27 12:35:03 +0900

    added header comment

--- helios_pcl/helios_pcl.cpp
+++ helios_pcl/helios_pcl.cpp
@@ -1,8 +1,25 @@
-﻿/*******
-***
-***Lucid Helios -> PCL
-**************/
+﻿/***************************************************************************************
+ ***                                                                                 ***
+ *** Example for getting PCL Type Point Cloud from a LUCID VISION Helios Device.     ***
+ *** Major portion has been adopted from a LUCID VISION Code Example.                ***
+ *** Regarding Copyright is stated below.                                            ***
+ *** Ahn, Jeeho                                                                      ***
+ ***                                                                                 ***
+ ***************************************************************************************/
 
+/***************************************************************************************
+ ***                                                                                 ***
+ ***  Copyright (c) 2021, Lucid Vision Labs, Inc.                                    ***
+ ***                                                                                 ***
+ ***  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     ***
+ ***  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       ***
+ ***  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    ***
+ ***  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         ***
+ ***  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  ***
+ ***  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  ***
+ ***  SOFTWARE.                                                                      ***
+ ***                                                                                 ***
+ ***************************************************************************************/
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/io/ply_io.h>
